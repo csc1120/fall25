f802ad475a4c080e4bda90427903111b9739024e
sean <jones@msoe.edu>
2025-09-18 09:14:37 -0500

    Quiz 3 comments added

--- src/week3/quiz3/Quiz3.java
+++ src/week3/quiz3/Quiz3.java
@@ -23,30 +23,42 @@ import java.util.Scanner;
 /**
  * A happy, joyful quiz full of unicorns and rainbows
  */
-public class Quiz3 extends Application {
+public class Quiz3 extends Application { // extends Application
     public static void main(String[] args) {
-        launch(args);
+        launch(args); //main method calls launch
     }
 
-    @Override
+    @Override // Overrides the abstract start method
     public void start(Stage stage) {
+        // get the file containing the button text
         Path buttons = Paths.get("data", "buttons.txt");
+        // make a parent container
         HBox hBox = new HBox();
+        // try with resources to read the file
         try(Scanner read = new Scanner(buttons)) {
             while(read.hasNextLine()) {
+                // for each line of text make a button using the text
                 Button b = new Button(read.nextLine());
+                // set a listener for the changeText
                 b.setOnAction(this::changeText);
+                // add the button to the children list of the container
                 hBox.getChildren().add(b);
             }
         } catch(IOException e) {
+            // show an Alert if there is an exception
             new Alert(Alert.AlertType.ERROR, "Could not load buttons").show();
         }
+        // add the container to the Scene and the Scene to the Stage
         stage.setScene(new Scene(hBox));
+        // Always give  your Stage a title
         stage.setTitle("Quiz 3");
+        // show the Stage!
         stage.show();
     }
 
+    // handler method should be private
     private void changeText(ActionEvent e) {
+        // Get a reference to the Button from the event object it created when clicked
         ((Button) e.getSource()).setText("clicked");
     }
 }
