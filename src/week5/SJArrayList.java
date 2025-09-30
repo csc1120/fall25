b305a0a0ebbce716b1d1e8fd66467eef4749790f
sean <jones@msoe.edu>
2025-09-30 09:16:24 -0500

    Updated SJArrayList

--- src/week5/SJArrayList.java
+++ src/week5/SJArrayList.java
@@ -25,7 +25,6 @@ public class SJArrayList<E> implements List<E> {
      * No param-constructor that defaults the starting capacity of the
      * backing array to 10
      */
-    @SuppressWarnings("unchecked")
     public SJArrayList() {
         this(DEFAULT_CAPACITY);
     }
@@ -35,6 +34,7 @@ public class SJArrayList<E> implements List<E> {
      * capacity of the list
      * @param startingCapacity the starting capacity of the list
      */
+    @SuppressWarnings("unchecked")
     public SJArrayList(int startingCapacity) {
         this.size = 0;
         this.data = (E[]) new Object[startingCapacity];
@@ -62,7 +62,7 @@ public class SJArrayList<E> implements List<E> {
 
     @Override
     public Iterator<E> iterator() {
-        return null;
+        throw new UnsupportedOperationException();
     }
 
     @Override
@@ -74,21 +74,39 @@ public class SJArrayList<E> implements List<E> {
     @Override
     public <T> T[] toArray(T[] a) {
         // Homework-adjacent problem
-        return null;
+        throw new UnsupportedOperationException();
     }
 
     @Override
-    public boolean add(E e) {
+    public boolean add(E e) { // O(1)
         if(this.size == this.data.length) {
-
-            // we got a problem. Need to make a bigger array
+            reallocate(); // make the backing array bigger
         }
-        this.data[size++] = e;
+        this.data[size++] = e; // increment size after adding
         return true;
     }
 
+    @SuppressWarnings("unchecked")
+    private void reallocate() { // O(n)
+        // make a bigger array
+        E[] temp = (E[]) new Object[this.data.length * 2 + 1];
+        System.arraycopy(this.data, 0, temp, 0, this.data.length);
+        this.data = temp;
+    }
+
     @Override
-    public boolean remove(Object o) {
+    public boolean remove(Object o) { // O(n)
+        // find it
+        for(int i = 0; i < this.size; ++i) {
+            if(this.data[i].equals(o)) {
+                // remove it
+                for(int j = i; j < this.size - 1; ++j) {
+                    this.data[j] = this.data[j + 1]; // shift all later indexes left
+                }
+                --this.size; // decrement size
+                return true;
+            }
+        }
         return false;
     }
 
@@ -118,28 +136,59 @@ public class SJArrayList<E> implements List<E> {
     }
 
     @Override
-    public void clear() {
-
+    @SuppressWarnings("unchecked")
+    public void clear() { // O(1)
+        this.data = (E[]) new Object[DEFAULT_CAPACITY];
+        this.size = 0;
     }
 
     @Override
-    public E get(int index) {
-        return null;
+    public E get(int index) throws IndexOutOfBoundsException { // O(1)
+        validateIndex(index); // validate the index
+        return this.data[index]; // return the element at the index
+    }
+
+    private void validateIndex(int index) throws IndexOutOfBoundsException {
+        if(index >= this.size || index < 0) { // O(1)
+            throw new IndexOutOfBoundsException("Index " + index + " is invalid for" +
+                    " a list of size " + this.size);
+        }
     }
 
     @Override
-    public E set(int index, E element) {
-        return null;
+    public E set(int index, E element) throws IndexOutOfBoundsException { // O(1)
+        validateIndex(index); // validate the index
+        E result = this.data[index]; // store the old element
+        this.data[index] = element; // set the new element
+        return result; // return the old element
     }
 
     @Override
-    public void add(int index, E element) {
-
+    public void add(int index, E element) throws IndexOutOfBoundsException {
+        // what is the Big-O?
+        validateIndex(index); // validate the index
+        if(this.size == this.data.length) { // make sure we have room
+            reallocate();
+        }
+        // shift all later elements to the right to make room for the new element
+        for(int i = this.size - 1; i >= index; --i) {
+            this.data[i + 1] = this.data[i];
+        }
+        this.data[index] = element; // add the element at the index
+        ++this.size; // increment size
     }
 
     @Override
-    public E remove(int index) {
-        return null;
+    public E remove(int index) throws IndexOutOfBoundsException {
+        // what is the Big-O?
+        validateIndex(index); // validate the index
+        E result = this.data[index];
+        // shift all the later elements left to close the gap
+        for(int i = index; i < this.size - 1; ++i) {
+            this.data[i] = this.data[i + 1];
+        }
+        --this.size; // decrement size
+        return result; // return the removed element
     }
 
     @Override
