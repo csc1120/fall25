4e997fdee54133fab54864decc7ff48241f72a5e
Sean Jones <jones@msoe.edu>
2025-10-08 07:35:21 -0500

    Updated 10-07-25

--- src/week6/SJLinkedList.java
+++ src/week6/SJLinkedList.java
@@ -11,6 +11,7 @@ import java.util.Collection;
 import java.util.Iterator;
 import java.util.List;
 import java.util.ListIterator;
+import java.util.function.Consumer;
 
 /**
  * Simple Linked List implementation
