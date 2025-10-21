27032e5c4dd12895e16fd66096305a0be86504da
sean <jones@msoe.edu>
2025-10-21 08:49:50 -0500

    102125 Lecture

--- src/week8/BinarySearch.java
+++ src/week8/BinarySearch.java
@@ -15,9 +15,26 @@ public class BinarySearch {
     public static void main(String[] args) {
         final int[] numbers = {2, 4, 6, 8, 10, 12, 14, 16, 18, 20, 22, 24};
         System.out.println(factorial(5));
-        System.out.println(binarySearch(numbers, 12));
+        System.out.println(recursiveBinarySearch(numbers, 2, 0, numbers.length - 1));
         System.out.println(binarySearch(numbers, 17));
         System.out.println(binarySearch(numbers, 2));
+    }
+
+    private static boolean recursiveBinarySearch(int[] data, int target,
+                                                 int begin, int end) {
+        // base case
+        if(begin >= end) {
+            return false;
+        }
+        // recursive case
+        int middle = (begin + end) / 2;
+        if(data[middle] == target) {
+            return true;
+        } else if(data[middle] < target) { // target is larger
+            return recursiveBinarySearch(data, target, middle + 1, end);
+        } else { // target is smaller
+            return recursiveBinarySearch(data, target, begin, middle);
+        }
     }
 
     private static boolean binarySearch(int[] data, int target) {
